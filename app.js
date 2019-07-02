f89880c23b8ef5e602d88bea12fb604a184d1057
ayush1999guptA <ayush2008rock@gmail.com>
2019-07-03 01:29:52 +0530

    hrnlo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express       = require("express"),
     mongoose      = require("mongoose"),
     seedDB        = require("./seeds");
 
-mongoose.connect("mongodb+srv://ayush1999guptA:therock@cluster0-tajvd.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true } );
+mongoose.connect("mongodb://ayush1999guptA:therock@cluster0-shard-00-00-tajvd.mongodb.net:27017,cluster0-shard-00-01-tajvd.mongodb.net:27017,cluster0-shard-00-02-tajvd.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",{ useNewUrlParser: true } );
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 app.use( require("express-session")({
